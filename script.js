85d82511ba960dc939ade12f2ee1f929f21c9fb1
Salman Shah <smsalmanshah@gmail.com>
2024-03-26 00:54:15 +0500

    chore add responsive mediaqueries

---
+++ script.js
@@ -0,0 +1,44 @@
+masonryImages=document.querySelectorAll('.masonryImage');
+imagesSection=document.querySelector('.images');
+root=document.documentElement;
+
+let totalImageHeight=10;
+
+for (image of masonryImages){
+    totalImageHeight += image.offsetHeight+10;
+}
+
+const fullheight= totalImageHeight;
+const col2height = 400+ fullheight/2;
+const col3height = 400+ fullheight/3;
+const col4height = 400+ fullheight/4;
+console.log('single column: ', fullheight);
+console.log('2 column: ', col2height);
+console.log('3 column: ', col3height);
+console.log('4 column: ', col4height);
+
+//when @650 2column
+
+// window.addEventListener('resize', ()=>{
+//     if (window.innerWidth < 650){
+//         console.log (`width=${window.innerWidth}`);
+//         imagesSection.style.height=`${fullheight}px`;
+//         console.log (`imageSectionHeight=${imagesSection.style.height}`);
+//     } else if (window.innerWidth >= 650  && window.innerWidth < 768 ){
+//         console.log (`width=${window.innerWidth}`);
+//         imagesSection.style.height=`${col2height}px`;
+//         console.log (`imageSectionHeight=${imagesSection.style.height}`);
+//     } else if (window.innerWidth >= 768 && window.innerWidth <1440){
+//         console.log (`width=${window.innerWidth}`);
+//         imagesSection.style.height=`${col3height}px`;
+//         console.log (`imageSectionHeight=${imagesSection.style.height}`);
+    
+//     }else if (window.innerWidth >= 768 && window.innerWidth <1440){
+//         console.log (`width=${window.innerWidth}`);
+//         imagesSection.style.height=`${col4height}px`;
+//         console.log (`imageSectionHeight=${imagesSection.style.height}`);
+    
+// }})
+
+
+
